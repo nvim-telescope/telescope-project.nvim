1acb5e351d0952b094672df05ac28f05cd20db9c
Chris Laidler <cjjlaidler@gmail.com>
2020-12-13 13:50:45 +0000

    feat: allow switching project (nvim config example)

--- lua/telescope/_extensions/project.lua
+++ lua/telescope/_extensions/project.lua
@@ -1,39 +1,56 @@
+local has_telescope, telescope = pcall(require, 'telescope')
+
+if not has_telescope then
+	error('This plugins requires nvim-telescope/telescope.nvim')
+end
 local actions = require("telescope.actions")
 local finders = require("telescope.finders")
 local pickers = require("telescope.pickers")
 local sorters = require("telescope.sorters")
+local builtin = require("telescope.builtin")
 
-return require("telescope").register_extension {
-    exports = {
-        configurations = function(opts)
-            opts = opts or {}
-            -- TODO check for existing projects
+ProjectDirectories = {["init.vim"] = "~/.config/nvim"}
 
-            if vim.tbl_isempty(configurations) then
-                return
-            end
+return telescope.register_extension {
+	exports = {
+		project = function(opts)
+			opts = opts or {}
 
-            pickers.new(
-                opts,
-                {
-                    prompt_title = "Project Launch",
-                    finder = finders.new_table {
-                        results = configurations
-                    },
-                    sorter = sorters.get_generic_fuzzy_sorter(),
-                    attach_mappings = function(prompt_bufnr, map)
-                        local search_project = function(prompt_bufnr, map)
-                            local selection = actions.get_selected_entry(prompt_bufnr)
-                            -- TODO start new search with cwd option
-                        end
+			local projects = {}
 
-                        map("i", "<CR>", search_project)
-                        map("n", "<CR>", search_project)
+			local search_selected_project = function(selection)
+				print(ProjectDirectories[selection])
+				local selected_directory = ProjectDirectories[selection]
+				builtin.find_files({cwd = selected_directory})
+			end
 
-                        return true
-                    end
-                }
-            ):find()
-        end
-    }
+			local n = 0
+			for k in pairs(ProjectDirectories) do
+				n = n + 1
+				projects[n] = k
+			end
+
+			local select_project = function()
+				pickers.new(opts, {
+					prompt_title = 'Select a project',
+					results_title = 'Projects',
+					finder = finders.new_table {results = projects},
+					sorter = sorters.get_generic_fuzzy_sorter(),
+					attach_mappings = function(prompt_bufnr, map)
+						local on_project_selected = function()
+							local selection = actions.get_selected_entry(prompt_bufnr)
+							actions.close(prompt_bufnr)
+							search_selected_project(selection.value)
+						end
+
+						map('i', '<CR>', on_project_selected)
+						map('n', '<CR>', on_project_selected)
+
+						return true
+					end
+				}):find()
+			end
+			select_project()
+		end
+	}
 }
