a21175f0346750aeaf795787d37681ddec3479e3
Chris Laidler <cjjlaidler@gmail.com>
2020-12-13 06:36:07 +0000

    fix: dodgy formatting!

--- lua/telescope/_extensions/project.lua
+++ lua/telescope/_extensions/project.lua
@@ -1,37 +1,39 @@
-local actions = require('telescope.actions')
-local finders = require('telescope.finders')
-local pickers = require('telescope.pickers')
-local sorters = require('telescope.sorters')
+local actions = require("telescope.actions")
+local finders = require("telescope.finders")
+local pickers = require("telescope.pickers")
+local sorters = require("telescope.sorters")
 
-return require('telescope').register_extension {
+return require("telescope").register_extension {
     exports = {
         configurations = function(opts)
             opts = opts or {}
-							-- TODO check for existing projects
+            -- TODO check for existing projects
 
-					if vim.tbl_isempty(configurations) then
-						return
-					end
+            if vim.tbl_isempty(configurations) then
+                return
+            end
 
-					pickers.new(opts, {
-						prompt_title = 'Project Launch',
-						finder = finders.new_table {
-							results = configurations
-						},
-						sorter = sorters.get_generic_fuzzy_sorter(),
-						attach_mappings = function(prompt_bufnr, map)
-							local search_project = function(prompt_bufnr, map)
-								local selection = actions.get_selected_entry(prompt_bufnr)
-								-- TODO start new search with cwd option
+            pickers.new(
+                opts,
+                {
+                    prompt_title = "Project Launch",
+                    finder = finders.new_table {
+                        results = configurations
+                    },
+                    sorter = sorters.get_generic_fuzzy_sorter(),
+                    attach_mappings = function(prompt_bufnr, map)
+                        local search_project = function(prompt_bufnr, map)
+                            local selection = actions.get_selected_entry(prompt_bufnr)
+                            -- TODO start new search with cwd option
+                        end
 
-							end
+                        map("i", "<CR>", search_project)
+                        map("n", "<CR>", search_project)
 
-							map('i', '<CR>', search_project)
-							map('n', '<CR>', search_project)
-
-							return true
-						end
-					}):find()
-				end
-			}
-		}
+                        return true
+                    end
+                }
+            ):find()
+        end
+    }
+}
