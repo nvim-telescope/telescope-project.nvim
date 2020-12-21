409aa91ea8f5d5dc9a1a8b681aa3d0f0aa56a22a
Chris Laidler <cjjlaidler@gmail.com>
2020-12-21 08:49:34 +0000

    feat: finish delete a project action

--- lua/telescope/_extensions/actions.lua
+++ lua/telescope/_extensions/actions.lua
@@ -15,23 +15,24 @@ end
 actions.delete_project = function(project_to_delete)
 	local project_dirs_without_deleted_project = {}
 	local file = assert(io.open(project_dirs_file, "w"), "No project file exists")
+	io.output(file)
 	for line in io.lines(project_dirs_file) do
 		local title, path = line:match("^(.-)=(.-)$")
 		if title ~= project_to_delete then
 			project_dirs_without_deleted_project[title] = path
+			io.write(title .. '=' .. path)
 		end
 	end
+	io.close()
 	print(project_dirs_without_deleted_project)
 end
 
-actions.open_path_at_selected_project = function(selection)
+actions.open_path_at_selected_project = function(selection, project_dirs)
 	vim.api.nvim_command('Explore ' .. project_dirs[selection])
 end
 
 actions.search_selected_project = function(selection)
-	print(selection.display);
-	actions.delete_project(selection.display);
-	-- builtin.find_files({cwd = selection.value})
+	builtin.find_files({cwd = selection.value})
 end
 
 return actions
