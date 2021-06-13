eb5d234cb60e1f5d8649ae3cfc8bd31d833cac4c
Logan Connolly <logan.connolly@protonmail.com>
2021-06-13 12:48:13 +0200

    test: add utils general tests

--- tests/project/utils_spec.lua
+++ tests/project/utils_spec.lua
@@ -1,10 +1,37 @@
-describe("general utils", function()
+describe("utils", function()
 
-  local path = require('plenary.path')
+  local path = require("plenary.path")
   local utils = require("telescope._extensions.project.utils")
 
-  it("init_file", function()
-    assert.equals(true, true)
+  describe("general", function()
+
+    it("trim whitespace (left and right)", function()
+      local input_str = " text "
+      assert.equal("text", utils.trim(input_str))
+    end)
+
+    it("string start with 'fatal'", function()
+      -- expected text when running git.try_and_find_git_path()
+      local input_str = "fatal: not a git repository (or any parent up to mount point /)"
+      assert.equal(true, utils.string_starts_with(input_str, "fatal"))
+    end)
+
+    it("table has a value (whitespace ignored)", function()
+      local paths = {"/projects/A", "/projects/B"}
+      assert.equal(true, utils.has_value(paths, "/projects/A"))
+      assert.equal(true, utils.has_value(paths, "/projects/B "))
+      assert.equal(false, utils.has_value(paths, "/projects/C"))
+    end)
+
+    it("check that /tmp/dummy.txt file exists", function()
+      local dummy_file = "/tmp/dummy.txt"
+      local dummy_path = path:new(dummy_file)
+
+      assert.equal(false, utils.file_exists(dummy_file))
+      dummy_path:touch()
+      assert.equal(true, utils.file_exists(dummy_file))
+    end)
+
   end)
 
 end)
