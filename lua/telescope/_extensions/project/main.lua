1e66392789c656b789ef3e7e8996abc9496dba10
Logan Connolly <logan.connolly@protonmail.com>
2021-06-15 08:53:00 +0200

    docs: update setup docstring

--- lua/telescope/_extensions/project/main.lua
+++ lua/telescope/_extensions/project/main.lua
@@ -16,7 +16,8 @@ local M = {}
 local base_dir
 local max_depth
 
--- Allow user to set base_dir in setup
+-- Setup function sets variables, cleans up missing projects,
+-- and updates projects via git project scanner
 M.setup = function(setup_config)
   base_dir = setup_config.base_dir or nil
   max_depth = setup_config.max_depth or 3
