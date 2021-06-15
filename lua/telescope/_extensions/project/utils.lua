d5bd0f016ce081e786164dce973fe6ca7aba7c60
Christopher Laidler <43636691+claidler@users.noreply.github.com>
2021-06-15 19:05:42 +0100

    Revert "feat: handle missing project paths (#37)" (#38)
    This reverts commit 46401464d94425574ab992e18c6dc23d02a36b83.

--- lua/telescope/_extensions/project/utils.lua
+++ lua/telescope/_extensions/project/utils.lua
@@ -1,5 +1,3 @@
-local Path = require('plenary.path')
-
 local M = {}
 
 -- The file path to telescope projects
@@ -7,7 +5,7 @@ M.telescope_projects_file = vim.fn.stdpath('data') .. '/telescope-projects.txt'
 
 -- Initialize file if does not exist
 M.init_file = function()
-  local file_path = Path:new(M.telescope_projects_file)
+  local file_path = require'plenary'.path:new(M.telescope_projects_file)
   if not file_path:exists() then
     file_path:touch()
   end
@@ -17,9 +15,8 @@ end
 M.get_projects = function()
   local filtered_projects = {}
   for _, project in pairs(M.get_project_objects()) do
-    local path_exists = Path:new(project.path):exists()
     local is_activated = tonumber(project.activated) == 1
-    if path_exists and is_activated then
+    if is_activated then
       table.insert(filtered_projects, project)
     end
   end
@@ -82,17 +79,6 @@ end
 M.store_project = function(file, project)
   local line = project.title .. "=" .. project.path .. "=" .. project.activated .. "\n"
   file:write(line)
-end
-
--- Remove project entries if they no longer exist in file system
-M.cleanup_missing_projects = function()
-  local file = io.open(M.telescope_projects_file, "w")
-  for _, project in pairs(M.get_project_objects()) do
-    if Path:new(project.path):exists() then
-      M.store_project(file, project)
-    end
-  end
-  io.close(file)
 end
 
 -- Trim whitespace for strings
