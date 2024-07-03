085f43519d7a4d84f045c691d8582a493f2b426f
Bruno <re4deto@hotmail.com>
2024-07-03 11:48:43 -0300

    Added the possibility to change the default mappings

--- lua/telescope/_extensions/project/main.lua
+++ lua/telescope/_extensions/project/main.lua
@@ -18,11 +18,38 @@ local base_dirs
 local hidden_files
 local order_by
 local on_project_selected
+local mappings = {
+  n = {
+    ['d'] = _actions.delete_project,
+    ['r'] = _actions.rename_project,
+    ['c'] = _actions.add_project,
+    ['C'] = _actions.add_project_cwd,
+    ['f'] = _actions.find_project_files,
+    ['b'] = _actions.browse_project_files,
+    ['s'] = _actions.search_in_project_files,
+    ['R'] = _actions.recent_project_files,
+    ['w'] = _actions.change_working_directory,
+    ['o'] = _actions.next_cd_scope,
+  },
+  i = {
+    ['<c-d>'] = _actions.delete_project,
+    ['<c-v>'] = _actions.rename_project,
+    ['<c-a>'] = _actions.add_project,
+    ['<c-A>'] = _actions.add_project_cwd,
+    ['<c-f>'] = _actions.find_project_files,
+    ['<c-b>'] = _actions.browse_project_files,
+    ['<c-s>'] = _actions.search_in_project_files,
+    ['<c-r>'] = _actions.recent_project_files,
+    ['<c-l>'] = _actions.change_working_directory,
+    ['<c-o>'] = _actions.next_cd_scope,
+    ['<c-w>'] = _actions.change_workspace,
+  }
+}
+
 
 -- Allow user to set base_dirs
 local theme_opts = {}
 M.setup = function(setup_config)
-
   if setup_config.base_dir then
     error("'base_dir' is no longer a valid value for setup. See 'base_dirs'")
   end
@@ -37,9 +64,17 @@ M.setup = function(setup_config)
   on_project_selected = setup_config.on_project_selected
   search_by = setup_config.search_by or "title"
   sync_with_nvim_tree = setup_config.sync_with_nvim_tree or false
-  local cd_scope = setup_config.cd_scope or {"tab", "window"}
+  local cd_scope = setup_config.cd_scope or { "tab", "window" }
   _actions.set_cd_scope(cd_scope)
   _git.update_git_repos(base_dirs)
+
+  local config_maps = setup_config.mappings or {}
+  for mode, maps in pairs(config_maps) do
+    maps = maps or {}
+    for keys, action in pairs(maps) do
+      mappings[mode][keys] = action
+    end
+  end
 end
 
 -- This creates a picker with a list of all of the projects
@@ -51,7 +86,6 @@ M.project = function(opts)
     finder = _finders.project_finder(opts, _utils.get_projects(order_by)),
     sorter = conf.file_sorter(opts),
     attach_mappings = function(prompt_bufnr, map)
-
       local refresh_projects = function()
         local picker = action_state.get_current_picker(prompt_bufnr)
         local finder = _finders.project_finder(opts, _utils.get_projects(order_by))
@@ -63,31 +97,11 @@ M.project = function(opts)
       _actions.delete_project:enhance({ post = refresh_projects })
       _actions.rename_project:enhance({ post = refresh_projects })
 
-      -- Project key mappings
-      map('n', 'd', _actions.delete_project)
-      map('n', 'r', _actions.rename_project)
-      map('n', 'c', _actions.add_project)
-      map('n', 'C', _actions.add_project_cwd)
-      map('n', 'f', _actions.find_project_files)
-      map('n', 'b', _actions.browse_project_files)
-      map('n', 's', _actions.search_in_project_files)
-      map('n', 'R', _actions.recent_project_files)
-      map('n', 'w', _actions.change_working_directory)
-      map('n', 'o', _actions.next_cd_scope)
-
-      map('i', '<c-d>', _actions.delete_project)
-      map('i', '<c-v>', _actions.rename_project)
-      map('i', '<c-a>', _actions.add_project)
-      map('i', '<c-A>', _actions.add_project_cwd)
-      map('i', '<c-f>', _actions.find_project_files)
-      map('i', '<c-b>', _actions.browse_project_files)
-      map('i', '<c-s>', _actions.search_in_project_files)
-      map('i', '<c-r>', _actions.recent_project_files)
-      map('i', '<c-l>', _actions.change_working_directory)
-      map('i', '<c-o>', _actions.next_cd_scope)
-
-      -- Workspace key mappings
-      map('i', '<c-w>', _actions.change_workspace)
+      for mode, maps in pairs(mappings) do
+        for keys, action in pairs(maps) do
+          map(mode, keys, action)
+        end
+      end
 
       local handler = function()
         if on_project_selected then
